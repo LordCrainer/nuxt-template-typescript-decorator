7e17d79f2a2c0d8636bd4495f23cb48e1ff0688a
LordCrainer <camogan3000@hotmail.com>
2021-05-29 22:06:33 -0500

    package: Add type/jest. test/Logo.spec.ts: change to relative path

--- src/ui/test/Logo.spec.ts
+++ src/ui/test/Logo.spec.ts
@@ -1,5 +1,6 @@
+// @ts-ignore
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Logo from '../components/Logo.vue'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
